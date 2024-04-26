c74d366447abd9b989fbce1a50b96c1864494f96
anna <anna.jansson@skola.taby.se>
2024-04-26 15:10:46 +0200

    boka

--- bowling.js
+++ bowling.js
@@ -1,6 +1,23 @@
 window.onload = function() {
     var img = document.getElementById('bakgrundbowling');
     img.onload = function() {
-      document.body.style.display = 'block'; 
+        document.body.style.display = 'block'; 
     };
-  };
+};
+
+document.addEventListener("DOMContentLoaded", function() {
+    const bookingForm = document.getElementById("bookingForm");
+    const messageDiv = document.getElementById("message");
+  
+    bookingForm.addEventListener("submit", function(event) {
+      event.preventDefault();
+  
+      const date = document.getElementById("date").value;
+      const time = document.getElementById("time").value;
+  
+      setTimeout(function() {
+        messageDiv.textContent = `Bokningen är ${date}, klockan ${time}`;
+      }, 1000);
+    });
+  });
+  
